e9bfe5b491a9aadc2e7bccd0cc2b664d6482c969
sho0331 <143997698+sho0331@users.noreply.github.com>
2023-09-24 07:01:12 +0000

    [finish]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get '/' => 'homes#top'
-  resources :books,except: [:new, :update, :show, :destroy]
-  delete '/books/:id(.:format)' => 'books#destroy', as: 'destroy_book'
-  get '/books/:id(.:format)' => 'books#show', as: 'show_book'
-  patch '/books/:id(.:format)' => 'books#update', as: 'update_book'
-  put '/books/:id(.:format)' => 'books#update'
+  #get '/' => 'homes#top'
+  root to: 'homes#top'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
